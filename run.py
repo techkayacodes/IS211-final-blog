e01eb55eec49d6b4e416b1de72cbbbc90358a30f
Tech Kaya <techkayacodes@gmail.com>
2023-12-17 23:00:11 -0500

    run.py opens flask app in local browser

--- run.py
+++ run.py
@@ -1,6 +1,22 @@
 from app import create_app
+import webbrowser
+import threading
+
+app = create_app()
+
+def open_browser():
+    webbrowser.open('http://127.0.0.1:5000')
+
+if __name__ == '__main__':
+    threading.Timer(1, open_browser).start()
+    app.run(debug=True)
+
+
+"""
+from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+"""
